6f4217de0523e051ac94e7c23603f4df0aa6492d
Paul Teng <plankp@outlook.com>
2019-02-07 11:41:38 -0500

    Update framework

--- YouMonster/src/main/java/org/atoiks/games/umst/App.java
+++ YouMonster/src/main/java/org/atoiks/games/umst/App.java
@@ -8,7 +8,7 @@ import java.awt.Font;
 import java.awt.FontFormatException;
 
 import org.atoiks.games.framework2d.FrameInfo;
-import org.atoiks.games.framework2d.swing.Frame;
+import org.atoiks.games.framework2d.java2d.Frame;
 
 import org.atoiks.games.umst.scenes.*;
 
